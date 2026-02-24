33db6846d2a6cfc6dcc2c4f1b05736c7eab5de95
p.vandenheuvel <peter.vd.heuvel@softpear.nl>
2026-02-24 01:17:27 +0100

    base project

---
+++ main/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "main";
+#define BLINK_GPIO 8
+
+static uint8_t s_led_state = 0;
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+void app_main(void)
+{
+    configure_led();
+
+    while (1) {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        gpio_set_level(BLINK_GPIO, s_led_state);
+        s_led_state = !s_led_state;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
